208e4a99ef1c519af4e4bca40907beef878f0ad4
Schlenker, Richard <richard.schlenker@exxeta.com>
2022-12-03 19:36:25 +0100

    Finished second riddle of day 3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 use std::fs;
-use std::str::Chars;
 use itertools::Itertools;
+use regex::Regex;
 
 struct Rucksack {
     first: HashSet<char>,
@@ -11,6 +11,12 @@ struct Rucksack {
 fn main() {
     let contents = fs::read_to_string("/Users/risc/dev/adventofcode/2022/day3/input.txt")
         .expect("Should have been able to read the file");
+
+    riddle_one(contents.clone());
+    riddle_two(contents)
+}
+
+fn riddle_one(contents: String) {
     let rucksacks: Vec<Rucksack> =
         contents.lines()
             .map(|line| line.split_at(line.len() / 2))
@@ -21,16 +27,42 @@ fn main() {
             .collect();
     let mut result: u32 = 0;
 
-    let mut container: Vec<char> = Vec::new();
-    for (index, rucksack) in rucksacks.iter().enumerate() {
-        println!("First: {}", rucksack.first.iter().join(""));
+    for rucksack in rucksacks.iter() {
         for character in rucksack.first.iter() {
             if rucksack.second.contains(character) {
                 result += get_priority(character)
             }
         }
     }
-    println!("End result is {}", result)
+    println!("First riddle result is {}", result)
+}
+
+//imo prettier solution using regex
+fn riddle_two(contents: String) {
+    let mut container: Vec<String> = Vec::new();
+    let lines: Vec<&str> = contents.lines().collect();
+    for (index, line) in lines.iter().enumerate() {
+        if index % 3 == 1 || index % 3 == 2 {
+            continue;
+        }
+        let regex = build_regex(&line);
+        let caps = regex.find_iter(lines[index + 1]).map(|hit| hit.as_str()).join("");
+        // let caps = regex.captures(lines[index + 1]).unwrap().iter().map(|m| m.map_or("", |a| a.as_str())).join("");
+        let regex = build_regex(&caps);
+        let caps = regex.find_iter(lines[index + 2]).map(|hit| hit.as_str()).reduce(|_a, b| b).unwrap();
+        // let caps = regex.captures(lines[index + 2]).unwrap().iter().map(|m| m.map_or("", |a| a.as_str())).join("");
+        container.push(String::from(caps));
+    }
+    let flatten = container.iter().map(|m| m.chars()).flatten();
+    let sum: u32 = flatten.map(|character| get_priority(&character)).sum();
+    println!("Second riddle result is {}", sum)
+}
+
+fn build_regex(line: &str) -> Regex {
+    let mut regex_string: String = "[".to_string();
+    regex_string.push_str(&line.to_string());
+    regex_string.push_str("]");
+    Regex::new(&regex_string).unwrap()
 }
 
 fn get_priority(character: &char) -> u32 {
